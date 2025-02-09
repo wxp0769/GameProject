335be7424f3dbdb24f0fd20a9fc0e6b2b5077bd0
wxp0769 <wxp0769.@qq.com>
2025-02-09 10:55:02 +0800

    2025-02-09 10:55:02使用python脚本更新

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>404 - 页面未找到</h1>
+      <p>抱歉，你访问的页面不存在。</p>
+      <a href="/" style={{ color: "blue", textDecoration: "underline" }}>返回首页</a>
+    </div>
+  );
+}
