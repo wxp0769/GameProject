a9fce62d57543b0401208d7298abd7a8c3d4d7e5
wxp0769 <wxp0769.@qq.com>
2025-02-18 19:57:52 +0800

    2025-02-18 19:57:51使用python脚本更新

--- game/utils/backup_restore.py
+++ game/utils/backup_restore.py
@@ -16,16 +16,15 @@ def backup_database():
     db_password = settings.DATABASES['default']['PASSWORD']
     db_host = settings.DATABASES['default']['HOST']
     db_port = settings.DATABASES['default']['PORT']
-    print(db_name, db_user, db_password, db_host,db_port)
 
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     backup_file = os.path.join(BACKUP_DIR, f'backup_{timestamp}.sql')
 
     # 执行 mysqldump 备份数据库
-    command = f"mysql -h {db_host} -P {db_port} -u {db_user} -p {db_password} {db_name} > {backup_file}"
+    command = f"mysqldump -h{db_host} -P{db_port} -u{db_user} -p{db_password} {db_name} > {backup_file}"
     print(os.system(command))
 
-    if os.system(command) == 1:
+    if os.system(command) == 0:
         return f"数据库备份成功: {backup_file}"
     else:
         return "数据库备份失败"
@@ -42,15 +41,14 @@ def restore_database(backup_file):
     db_port = settings.DATABASES['default']['PORT']
 
     backup_path = os.path.join(BACKUP_DIR, backup_file)
-    print(backup_path, backup_file)
 
     if not os.path.exists(backup_path):
         return "Backup file not found"
 
     # 执行 mysql 进行数据库恢复
-    command = f"mysql -h {db_host} -P {db_port} -u {db_user} -p'{db_password}' {db_name} < {backup_path}"
+    command = f"mysql -h {db_host} -P {db_port} -u {db_user} -p{db_password} {db_name} < {backup_path}"
 
-    if os.system(command) == 1:
+    if os.system(command) == 0:
         return f"数据库恢复成功: {backup_file}"
     else:
         return "数据库恢复失败"
